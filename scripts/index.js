80d2fc7b3908617173f85c8c263a0de5c2fd600f
Augusto Mariutti <augusto.mariutti94@gmail.com>
2021-12-07 20:09:56 -0300

    mod login

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,32 @@
-var cart = []
-function addToCart(obj){
-    /*-Esta funcion va a servir para añadir articulos al carrito
-    -"obj" va a ser el parametro que va a usar para agregar el objeto al carrito*/
-    cart.push(obj)
-}
-function login(user, password){
+var Cart = {
+    content: [], 
+    owner: '',
+    
+    addToCart(obj){
+        /*-Esta funcion va a servir para añadir articulos al carrito
+        -"obj" va a ser el parametro que va a usar para agregar el objeto al carrito*/
+        this.content.push(obj)
+
+    },
+    setOwner(owner){
+        this.owner = owner;
+    }
+} 
+function loginCheck(user, password){
     /*- la funcion sirve para poder loguearse con el usuario y contraseña predefinidos*/
     if (user == "augusto" && password == "petiruscomepizza"){
-     return true   
+     return true 
     }
     return false
+}
+function login(res){
+    if (loginCheck(res.user, res.password)){
+        Cart.setOwner(res.user);
+        console.log('usuario logueado ');
+    }
+    else{
+        console.log('usuario incorrecto');
+    }
 }
 class Articulo{
     constructor (nombre, marca, precio, stock){
@@ -25,3 +42,4 @@ class Articulo{
     }
 }
 const producto1 = new Articulo ("Placa de video rtx 3060", "MSI", "135000",54);
+
