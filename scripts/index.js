02d796d00b45b39558f53e601c2848d3a0d2f120
Augusto Mariutti <augusto.mariutti94@gmail.com>
2021-12-28 19:55:50 -0300

    mod navbar, cart and fonts

--- scripts/index.js
+++ scripts/index.js
@@ -42,7 +42,7 @@ class Articulo{
 
     }
 }
-const productos = [ new Articulo ("Placa de video rtx 3060", "MSI",'https://asset.msi.com/resize/image/global/product/product_1610444725b6aa81c4e74a8ea9fee28fdd225b58cc.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png', "135000",54), new Articulo ("Rtx 3060 TI", "MSI",'https://mexx-img-2019.s3.amazonaws.com/40461_1.jpeg', "185000",50), new Articulo ("Gabineta K20", "Sentey",'https://d2r9epyceweg5n.cloudfront.net/stores/001/381/842/products/k20_011-adeaaa2dbffd4de97f16100722228928-1024-1024.jpg', "25000",10)];
+const productos = [ new Articulo ("RTX 3060", "MSI",'https://asset.msi.com/resize/image/global/product/product_1610444725b6aa81c4e74a8ea9fee28fdd225b58cc.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png', "135000",54), new Articulo ("RTX 3060 TI", "MSI",'https://mexx-img-2019.s3.amazonaws.com/40461_1.jpeg', "185000",50), new Articulo ("SENTEY K20", "Sentey",'https://d2r9epyceweg5n.cloudfront.net/stores/001/381/842/products/k20_011-adeaaa2dbffd4de97f16100722228928-1024-1024.jpg', "25000",10)];
 
 function crearArticulos(articulos){
     for (const articulo of articulos){
